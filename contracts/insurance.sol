2714a6f1138a6492f68750ad05c8a17ab7eceff4
Nadeem Bhati <nadeemb53@gmail.com>
2019-12-06 09:40:14 +0530

    removed bytes function

--- contracts/insurance.sol
+++ contracts/insurance.sol
@@ -81,28 +81,6 @@ contract insurance {
         return insuranceId;
     }
 
-    function convertIntToString(uint256 _number) internal pure returns (string memory) {
-        uint256 _tmpN = _number;
-
-        if (_tmpN == 0) {
-            return "0";
-        }
-
-        uint256 j = _tmpN;
-        uint256 length = 0;
-        while (j != 0){
-            length++;
-            j /= 10;
-        }
-        bytes memory bstr = new bytes(length);
-        uint256 k = length - 1;
-        while (_tmpN != 0) {
-            bstr[k--] = byte(48 + _tmpN % 10);
-            _tmpN /= 10;
-        }
-        return string(bstr);
-    }
-
     function getPolicy(uint _insuranceId) public view returns(uint, uint, string memory, string memory) {
         policy storage p = policymapping[_insuranceId];
         return (
